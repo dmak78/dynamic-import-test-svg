74d49c5a8d90b435a6ffd6bc62efa122e34d11bc
Kevin Bleich <dmak78@gmail.com>
2022-02-01 11:25:19 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Icon from "./Icon";
+export default function App() {
+  return (
+    <div className="App">
+      <Icon name="icon" />
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
